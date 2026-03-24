f2852b408f2a4e01f27e3e4a82f4b2382db29827
Mason <mirabito.mason@gmail.com>
2026-03-24 16:45:51 +1030

    cleaned up service worker and cache '/'

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -4,9 +4,6 @@ const CACHE = "pwabuilder-offline-page";
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
-const offlineFallbackPage = "offline-fallback.html";
-
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
     self.skipWaiting();
@@ -16,7 +13,9 @@ self.addEventListener("message", (event) => {
 self.addEventListener('install', async (event) => {
   event.waitUntil(
     caches.open(CACHE)
-      .then((cache) => cache.add(offlineFallbackPage))
+      .then((cache) => cache.addAll([
+        '/',
+      ]))
   );
 });
 
