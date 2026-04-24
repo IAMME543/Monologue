35e191976b571e325d0d65a0d87696f04565a47d
Mason <mirabito.mason@gmail.com>
2026-04-24 17:41:38 +0930

    refactor copy so that images work better and convert to png

--- index.js
+++ index.js
@@ -49,19 +49,40 @@ postIn.addEventListener('paste', (e) => {
         }
     }
 })
-copy.addEventListener('click', (e) => {
-    const content = state.selectedPost.querySelector(':scope > p').innerHTML
-
-    const htmlBlob = new Blob([content], { type: "text/html" });
-    const plainBlob = new Blob([content], { type: "text/plain" });
-    const clipboardItem = new ClipboardItem({
-        "text/html": htmlBlob,
-        "text/plain": plainBlob
-    });
-    navigator.clipboard.write([clipboardItem]);
-    hamburgerMenu.style.display = 'none'
+copy.addEventListener('click', async (e) => {
+    hamburgerMenu.style.display = 'none';
     e.stopPropagation();
-})
+
+    const raw = state.selectedPost.querySelector(':scope > p').innerHTML;
+
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(raw, 'text/html');
+
+    const cleanedHTML = doc.body.innerHTML;
+
+    const img = doc.querySelector('img')
+    const src = img.src;
+    const res = await fetch(src);
+    const imgBlob = await res.blob();
+
+    const pngBlob = toPNG(imgBlob)
+
+    img.remove();
+    const parent = img.parentElement;
+    if (parent && parent.style.textAlign === 'center' && parent.children.length === 0) {
+        parent.remove();
+    }
+
+    let clipboardItems = {
+        "text/html": new Blob([cleanedHTML], { type: "text/html" }),
+        "text/plain": new Blob([doc.body.textContent], { type: "text/plain" }),
+        "image/png": pngBlob
+    };
+
+    await navigator.clipboard.write([
+        new ClipboardItem(clipboardItems)
+    ]);
+});
 remove.addEventListener('click', (e) => {
     postData = state.selectedPost.postData;
     postData.visible = false;
@@ -340,4 +361,23 @@ function openMenu(btn) {
         hamburgerMenu.style.left = `${rect.right + window.scrollX}px`;
     }
     state.selectedPost = btn.parentElement.parentElement;
+}
+
+async function toPNG(blob) {
+
+    if (blob.type == "image/png") {
+        return blob;
+    }
+    const bitmap = await createImageBitmap(blob);
+
+    const canvas = document.createElement('canvas');
+    canvas.width = bitmap.width;
+    canvas.height = bitmap.height;
+
+    const ctx = canvas.getContext('2d');
+    ctx.drawImage(bitmap, 0, 0);
+
+    return new Promise(resolve => {
+        canvas.toBlob(resolve, 'image/png');
+    });
 }
